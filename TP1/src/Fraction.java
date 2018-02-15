0a49c1e3fd95ae247af3e2eedf8541c3c2c7c4bb
anass05 <anassda99@gmail.com>
2018-02-15 01:45:41 +0000

    ajout des commantaires

--- TP1/src/Fraction.java
+++ TP1/src/Fraction.java
@@ -1,28 +1,52 @@
 import java.math.BigInteger;
 
 /**
- * Created by Anass on 2018-02-12.
- * Updated now
+ * Classe Fraction qui represente une fraction
+ * @author Anass
  */
 public class Fraction {
-
+    /**
+     *Une fraction est définie par:
+     * un numérateur: num de type BigInteger
+     * un dénominateur: den de type BigInteger
+     */
     private BigInteger num;
     private BigInteger den;
 
+    /**
+     * Le constructeur
+     * @param num qui est le numérateur de type BigInteger
+     * @param den qui est le dénominateur de type BigInteger
+     * */
     public Fraction(BigInteger num, BigInteger den) {
         this.num = num;
         this.den = den;
     }
 
+    /**
+     *Le deuxiéme constructeur qui fait appelle au premier constructeur
+     * avec une conversion des parametre en BigInt
+     * @param num qui est le numérateur de type int
+     * @param den qui est le dénominateur de type int
+     * */
+
     public Fraction(int num, int den) {
         this(BigInteger.valueOf(num),BigInteger.valueOf(den));
 
     }
-
+    /**
+     *Le troisiere constructeur qui fait appelle au deuxiéme constructeur
+     * avec une conversion du parametre num en BigInt et le den est initialisé à 1
+     * @param num qui est le numérateur de type int
+     * */
     public Fraction(int num) {
         this(num,1);
     }
 
+    /**
+     * La fonction d'ajout qui sert a ajouter deux fractions
+     * @param f la fraction à ajouter à la fraction appelant la méthode
+     * */
     public Fraction add(Fraction f){
         BigInteger top = BigInteger.valueOf(0);
         BigInteger bottom = BigInteger.valueOf(1);
@@ -33,6 +57,10 @@ public class Fraction {
         return new Fraction(top,bottom);
     }
 
+    /**
+     * La fonction de soustraction qui sert a soustraire deux fractions
+     * @param f la fraction à soustraire de la fraction appelant la méthode
+     * */
     public Fraction sub(Fraction f){
         BigInteger top = BigInteger.valueOf(0);
         BigInteger bottom = BigInteger.valueOf(1);
@@ -43,6 +71,10 @@ public class Fraction {
         return new Fraction(top,bottom);
     }
 
+    /**
+     * La fonction de multiplication qui sert a multiplier deux fractions
+     * @param f la fraction à multiplier avec la fraction appelant la méthode
+     * */
     public Fraction mult(Fraction f){
         BigInteger top = BigInteger.valueOf(1);
         BigInteger bottom = BigInteger.valueOf(1);
@@ -51,6 +83,10 @@ public class Fraction {
         return new Fraction(top,bottom);
     }
 
+    /**
+     * La fonction de division qui sert a diviser deux fractions
+     * @param f la fraction à diviser de la fraction appelant la méthode
+     * */
     public Fraction divi(Fraction f){
         BigInteger top = BigInteger.valueOf(1);
         BigInteger bottom = BigInteger.valueOf(1);
@@ -59,11 +95,17 @@ public class Fraction {
         return new Fraction(top,bottom);
     }
 
+    /**
+     * La fonction qui retourn un String affichant l'objet
+     * */
     @Override
     public String toString() {
         return num+"/"+den;
     }
 
+    /**
+     * Le getters et le setters
+     * */
     public BigInteger getNum() {
         return num;
     }
